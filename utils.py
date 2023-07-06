0379e24b64cd51aa555fd520a2f925ce8f36b42e
Grigor-code <129864086+Grigor-code@users.noreply.github.com>
2023-07-06 09:46:43 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,24 +1,78 @@
 import config
-import urllib2
+import urllib.request
 import json
 import time
-import thread
+import _thread
 from time import sleep
+import openai
+import os
+from revChatGPT.V1 import Chatbot
+import random
+
+chatbot=Chatbot(config={"access_token":config.TOKEN})
+openai.api_key=config.TOKEN
+engine="text-davinci-003"
+
+def chatGTP(sock,message):
+    completion = openai.Completion.create(engine=engine,
+                                          prompt=message,
+                                          temperature=0.5,
+                                          max_tokens=1000)
+    string=str(completion.choices[0]['text'])
+    no_dots_string = string.replace("\n", "")
+    split_text = [no_dots_string[i:i + 255] for i in range(0, len(no_dots_string), 255)]
+
+    for i in range (0,len(split_text)):
+        mess(sock, split_text[i])
+
 
 
 
 def mess(sock, message):
-    sock.send("PRIVMSG #{} :{}\r\n".format(config.CHAN, message))
+    msg = "PRIVMSG #" + config.CHAN + " :" + message + "\r\n"
+    sock.send(msg.encode())
 
 
+def roll(sock, user):
+    a=random.randint(1,100)
+    if user:
+        result = user.group()
+    mess(sock, "@{}".format(result) + " заролил {}".format(a))
 
 def ban(sock, user):
     mess(sock, ".ban {}".format(user))
 
+def randomhero(sock,user):
+    random_hero=random.choice(config.heroes)
+    if user:
+        result = user.group()
+    mess(sock, "@{}".format(result) + " сегодня играет, например, на {}".format(random_hero))
+
+def radom(sock, user):
+    a=random.randint(1,5)
+    if user:
+        result = user.group()
+
+    if a==1:
+        mess(sock, "@{}".format(result) + " повезло, но почему рот в говне")
+
+
+    else :
+        mess(sock, "@{}".format(result) + " неудачник проебал жопу")
+        
+
+
+
+
 
 
 def timeout(sock, user, seconds = 500):
-    mess(sock, ".timeout {}".format(user, seconds))
+    command = f".timeout {user} {seconds}"
+    mess(sock, command)
+
+
+def caca(sock):
+        mess(sock,"СТРИМ ЗАКОНЧЕН")
 
 
 
@@ -28,8 +82,8 @@ def fillOpList():
     while True:
         try:
             url = "http://tmi.twitch.tv/group/user/winderton/chatters"
-            req = urllib2.Request(url, headers={"accept": "*/*"})
-            res = urllib2.urlopen(req).read()
+            req = urllib.Request(url, headers={"accept": "*/*"})
+            res = urllib.urlopen(req).read()
             if res.find("502 bad gateway") == - 1:
                 config.oplist.clear()
                 data = json.loads(res)
@@ -44,9 +98,6 @@ def fillOpList():
         except:
             "Something went wrong...do nothing"
         sleep(5)
-
-
-
 def isOp(user):
     return user in config.oplist
 
